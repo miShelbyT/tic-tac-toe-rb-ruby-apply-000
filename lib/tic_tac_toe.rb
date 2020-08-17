92cb8dbd2e90f6f0e40fbbdf8df6eed54c04fb70
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-17 15:27:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ def turn(board)
     if valid_move?(board, index)
       move(board, index, current_player(board))
       display_board(board)
-      turn(board)
+      #turn(board)
     end
     display_board(board)
 end
